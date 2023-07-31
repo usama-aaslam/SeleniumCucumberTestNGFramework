66b0cea4c8e1c4b835be9def5fabb91dc7ad7b01
usama-aslam1 <129889121+usama-aslam1@users.noreply.github.com>
2023-07-31 11:26:24 +0500

    Add files via upload

---
+++ CucumberBDDFramework/src/main/java/Utilities/TestUtil.java
@@ -0,0 +1,7 @@
+package Utilities;
+
+//Test utilities are define here and use in the Base Test
+public class TestUtil {
+    static int PAGE_LOAD_TIMEOUT = 20;
+    static int IMPLICIT_WAIT = 30;
+}
---
+++ CucumberBDDFramework/src/main/java/stepDefinition/CheckoutProductStepDefinition.java
@@ -0,0 +1,80 @@
+package stepDefinition;
+
+import Pages.Checkout;
+import Pages.HomePage;
+import Pages.LoginPage;
+import Utilities.BaseTest;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+public class CheckoutProductStepDefinition extends BaseTest {
+    LoginPage loginPage;
+    HomePage homePage;
+
+    Checkout checkout;
+
+    @Given("^user open the browser$")
+    public void user_open_the_browser() {
+        BaseTest.intialization();
+
+    }
+
+    @Then("^user is on login page$")
+    public void user_is_on_login_page() {
+        loginPage = new LoginPage();
+        String title = loginPage.validateLoginPageTitle();
+        Assert.assertEquals( "Swag Labs", title );
+
+    }
+
+    @Then("^user logs into the application$")
+    public void user_enters_username_and_password() {
+        homePage = loginPage.login( properties.getProperty( "username" ), properties.getProperty( "password" ) );
+    }
+
+
+    @Then("^user validate the home page$")
+    public void user_validate_the_home_page() {
+        String homePageTitle = homePage.verifyHomePageTitle();
+        Assert.assertEquals( "Products" , homePageTitle );
+    }
+
+    @And("^user add products to cart$")
+    public void user_add_products_to_cart() {
+        homePage.addProductToCart();
+    }
+
+    @Then("^validate product added to cart successfully$")
+    public void validate_product_added_to_cart_successfully() {
+        String validateCartCount = homePage.validateProductAdded();
+        Assert.assertEquals( "2" , validateCartCount );
+    }
+
+    @Given("^user go to cart$")
+    public void user_go_to_cart() {
+        checkout = new Checkout();
+        checkout.GoToCart();
+
+    }
+    @Then("^user enter checkout information$")
+    public void user_enter_checkout_information() {
+
+    }
+    @Then("^user finish and checkout$")
+    public void user_finish_and_checkout() {
+
+    }
+    @Then("^validate user checkout successfully$")
+    public void validate_user_checkout_successfully() {
+
+    }
+
+   // @Then( "^close browser$" )
+    public void close_browser(){
+
+        driver.quit();
+    }
+
+}
