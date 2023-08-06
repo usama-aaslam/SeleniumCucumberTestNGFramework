c1dce6913f856c60e6cec563271ceb2c8e33880c
Usama Aslam <129889121+usama-aaslam@users.noreply.github.com>
2023-08-06 11:57:20 +0500

    Delete CucumberBDDFramework directory
    Directory Deleted

--- CucumberBDDFramework/src/main/java/Utilities/TestUtil.java
+++
@@ -1,7 +0,0 @@
-package Utilities;
-
-//Test utilities are define here and use in the Base Test
-public class TestUtil {
-    static int PAGE_LOAD_TIMEOUT = 20;
-    static int IMPLICIT_WAIT = 30;
-}
